c4f8246183e7aa616fa8eaabe59e7f4ea068f868
TanakaMafuta <145983339+TanakaMafuta@users.noreply.github.com>
2025-07-21 23:20:00 +0200

    Update config.js

--- config.js
+++ config.js
@@ -213179,7 +213179,7 @@ module.exports = {
     MENU_STATS_LINE: process.env.MENU_STATS_LINE || "│⭐ ",
     MENU_BOTTOM_DIVIDER: process.env.MENU_BOTTOM_DIVIDER || "└─────────────┈⳹",
     
-    FOOTER: process.env.BOT_FOOTER || '\n\nFor more info visit: bwmxmd.online\n\n®2025 ʙᴡᴍ xᴍᴅ 🔥',
+    FOOTER: process.env.BOT_FOOTER || '\n\nFor more info call cyber-ghost: \n\n®2025 CYBER-GHOST 🔥',
     DATABASE_URL,
     DATABASE: DATABASE_URL === databasePath
         ? "postgresql://postgres:bKlIqoOUWFIHOAhKxRWQtGfKfhGKgmRX@viaduct.proxy.rlwy.net:47738/railway"
